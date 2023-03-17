a2f7bb67a2e5490b88ba0c149acf8da993dcd785
Aigend <90514843+Aigend@users.noreply.github.com>
2023-03-17 21:25:04 +0800

    Update 0317_gevent.py

--- 0317_gevent.py
+++ 0317_gevent.py
@@ -41,3 +41,29 @@ print("%%%")
 g3.join()
 
 # gevent.joinall([g1,g2, g3])
+
+print("******************")
+
+import gevent
+from gevent import monkey
+import urllib.request
+monkey.patch_all()
+
+
+def downloader(file_name, url_address):
+    req = urllib.request.urlopen(url_address)
+    content = req.read()
+    with open(file_name, "wb") as f:
+        f.write(content)
+
+
+def main():
+    gevent.joinall([
+        gevent.spawn(downloader, "绝地求生女解说Corrine宅个人Vlog视频.mp4",
+                                 "https://img.dongyoutu.com/20210423/841dd2e192db6c70630ba12112c386f8.mp4"),
+        gevent.spawn(downloader, "《跑跑卡丁车：RUSH+》与《绝地求生Mobile》一起联手推出搞笑视频.mp4", "https://img.dongyoutu.com/20210402/1111.mp4")
+                    ])
+
+
+if __name__ == '__main__':
+    main()
