eede0b4821a6fe7d04717f295de520befea8b937
Aigend <90514843+Aigend@users.noreply.github.com>
2023-04-09 09:35:12 +0800

    Create 709. 转换成小写字母.py

---
+++ leetcode/709. 转换成小写字母.py
@@ -0,0 +1,8 @@
+给你一个字符串 s ，将该字符串中的大写字母转换成相同的小写字母，返回新的字符串。
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        # res = []
+        # for v in s:
+        #     res.append(v.lower())
+        # return "".join(res)
+        return s.lower()
