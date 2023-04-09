c1e43f4f437aa5e6f3e027abdc65345656c25131
Aigend <90514843+Aigend@users.noreply.github.com>
2023-04-09 11:10:27 +0800

    Create 283. 移动零.py

---
+++ leetcode/283. 移动零.py
@@ -0,0 +1,28 @@
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # left = 0
+        # right = len(nums)-1
+        # while left < right:
+        #     while left < right and nums[left]!=0:
+        #         left += 1
+        #     while left < right and nums[right]==0:
+        #         right -= 1
+        #     if left < right:
+        #         nums[left], nums[right]=nums[right], nums[left]
+        # return nums
+        ret = 0
+        j = 0
+        for i in range(len(nums)):
+            if nums[i]!= 0:
+                ret+=1
+                nums[j]=nums[i]
+                j+=1
+        for i in range(ret, len(nums)):
+            nums[i]=0
+        return nums
