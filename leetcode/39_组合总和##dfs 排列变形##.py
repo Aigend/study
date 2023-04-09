5df787a24dd6a15739546c78da0bf507e1262c47
Aigend <90514843+Aigend@users.noreply.github.com>
2023-04-09 22:18:54 +0800

    Update and rename 39. 组合总和<dfs 排列变形>.py to 39_组合总和##dfs 排列变形##.py

--- leetcode/39_组合总和##dfs 排列变形##.py
+++ leetcode/39_组合总和##dfs 排列变形##.py
@@ -1,4 +1,7 @@
-给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+39_组合总和##dfs 排列变形##
+
+给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。
+你可以按 任意顺序 返回这些组合。
 
 candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。 
 
@@ -48,6 +51,3 @@ class Solution:
         size = len(candidates)
         dfs(candidates, target, result, path, index, size)
         return result
-      
-     
-
