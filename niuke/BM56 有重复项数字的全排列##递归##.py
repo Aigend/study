ae545fc079b8d2f96d8257d1c411a8a4a4be70d5
Aigend <90514843+Aigend@users.noreply.github.com>
2023-06-11 11:04:30 +0800

    Update BM56 有重复项数字的全排列##递归##.py

--- niuke/BM56 有重复项数字的全排列##递归##.py
+++ niuke/BM56 有重复项数字的全排列##递归##.py
@@ -4,7 +4,6 @@
 from copy import copy
 from collections import Counter
 
-
 class Solution:
     def permuteUnique(self, num):
         # write code here
@@ -23,5 +22,32 @@ class Solution:
 
         trackback(track, Counter(num))
         return result
-
+    
+import copy
+class Solution:
+    def permuteUnique(self , num: List[int]) -> List[List[int]]:
+        # write code here
+        n = len(num)
+        res = []
+        tmp = []
+        m = {}
+        num.sort()
+        for k in num:
+            m[k]= m.get(k, 0) + 1
+        def dfs(n, num, tmp, res, m):
+            if len(tmp) == n:
+                res.append(copy.copy(tmp))
+                return
+            print(m, tmp)
+            for k, v in m.items():
+                if v > 0:
+                    tmp.append(k)
+                    print(v, m[k])
+                    m[k] = v-1  # 这里写成 v -1 就不行，没太明白
+                    dfs(n, num, tmp, res, m)
+                    tmp.pop()
+                    m[k] = v+ 1
+        dfs(n, num, tmp, res, m)
+        return res
+    
 print(Solution().permuteUnique([1, 1, 2]))
