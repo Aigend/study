286b44a7ec0a29844f6f2ca548a5b56846632466
Aigend <90514843+Aigend@users.noreply.github.com>
2023-06-11 11:03:20 +0800

    Update BM55 没有重复项数字的全排列##递归##.py

--- niuke/BM55 没有重复项数字的全排列##递归##.py
+++ niuke/BM55 没有重复项数字的全排列##递归##.py
@@ -1,24 +1,46 @@
 # @File    : BM55 没有重复项数字的全排列.py
 # @Date    : 2022/09/05:20:09
-
-from copy import copy
+import copy
+from collections import Counter
 class Solution:
-    def permute(self , num):
+    def permute(self , num: List[int]) -> List[List[int]]:
         # write code here
-        result = []
-        track = []
-        def trackback(track, num, result):
-            if len(track) == len(num):
-                result.append(copy(track))
-                return
-            for i in num:
-                if i in track:
-                    continue
-                track.append(i)
-                trackback(track, num, result)
-                track.pop()
-        trackback(track, num, result)
-        return result
+        # 第一种方法
+        # res = []
+        # tmp = []
+        # s = set()
+        # n = len(num)
+        # def dfs(n, num, s, tmp, res):
+        #     if len(tmp) == n:
+        #         res.append(copy.copy(tmp))
+        #         return
+        #     for k in num:
+        #         if k in s:
+        #             continue
+        #         tmp.append(k)
+        #         s.add(k)
+        #         dfs(n, num, s,tmp, res)
+        #         tmp.pop()
+        #         s.remove(k)
+        # dfs(n, num, s, tmp, res)
+        # return res
 
-if __name__ == '__main__':
-    print(Solution().permute([1, 2, 3]))
+        # 第二种
+        res = []
+        tmp = []
+        n = len(num)
+        num.sort()
+        counter = Counter(num)
+        def dfs(res, tmp, n, counter):
+            if len(tmp) == n:
+                res.append(copy.copy(tmp))
+                return
+            for v in counter:
+                if counter[v]:
+                    tmp.append(v)
+                    counter[v] -= 1
+                    dfs(res, tmp, n, counter)
+                    tmp.pop()
+                    counter[v] += 1
+        dfs(res, tmp, n, counter)
+        return res
